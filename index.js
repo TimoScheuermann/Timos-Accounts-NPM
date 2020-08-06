5828c9caaab6dd1351835e4f926a1420f3b5bedc
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-08-06 15:56:56 +0200

    0.0.1

---
+++ index.js
@@ -0,0 +1,11 @@
+export function signIn() {
+  console.log('Sign in');
+}
+
+export function signOut() {
+  console.log('Sign out');
+}
+
+export function getTAUser() {
+  console.log('TA User');
+}
